37e409bdb803f4522e808a3103f268b0ddb104bb
James Brink <brink.james@gmail.com>
2018-04-14 13:00:51 -0700

    Updated run context of build hooks for docker

--- build.sh
+++ build.sh
@@ -3,17 +3,20 @@
 case "$1" in
     builder)
         export IMAGE_NAME="emscripten:builder"
-        exec ./Dockerfile.stages/builder/hooks/build
+        cd ./Dockerfile.stages/builder
+        exec ./hooks/build
     ;;
     z3)
         export IMAGE_NAME="emscripten:z3"
         export FROM_IMAGE="emscripten:builder"
-        exec ./Dockerfile.stages/z3/hooks/build
+        cd ./Dockerfile.stages/z3
+        exec ./hooks/build
     ;;
     fastcomp)
         export IMAGE_NAME="emscripten:fastcomp"
         export FROM_IMAGE="emscripten:builder"
-        exec ./Dockerfile.stages/fastcomp/hooks/build
+        cd ./Dockerfile.stages/fastcomp
+        exec ./hooks/build
     ;;
     *)
         exit 1
