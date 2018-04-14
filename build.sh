d2665ff95d9a9ed6b88911b9a1c38c4b45e38ad2
James Brink <brink.james@gmail.com>
2018-04-14 05:32:49 -0700

    Created build hooks for each stage

--- build.sh
+++ build.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-IMAGE_NAME="emscripten"
-exec ./hooks/build "${@}"
+case "$1" in
+    builder)
+        export IMAGE_NAME="emscripten:builder"
+        exec ./Dockerfile.stages/builder/hooks/build
+    ;;
+    z3)
+        export IMAGE_NAME="emscripten:z3"
+        export FROM_IMAGE="emscripten:builder"
+        exec ./Dockerfile.stages/z3/hooks/build
+    ;;
+    fastcomp)
+        export IMAGE_NAME="emscripten:fastcomp"
+        export FROM_IMAGE="emscripten:builder"
+        exec ./Dockerfile.stages/fastcomp/hooks/build
+    ;;
+    *)
+        exit 1
+    ;;
+esac
